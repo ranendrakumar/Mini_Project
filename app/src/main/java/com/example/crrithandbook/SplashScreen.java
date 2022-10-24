78485989487d4a2afd2669de8972277f86687e3f
ranendrakumar <ranendracrrit@gmail.com>
2022-10-24 09:21:20 +0530

    mini project

--- app/src/main/java/com/example/crrithandbook/SplashScreen.java
+++ app/src/main/java/com/example/crrithandbook/SplashScreen.java
@@ -6,9 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Toast;
@@ -28,6 +31,13 @@ public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.primary));
+            // window.setNavigationBarColor(this.getResources().getColor(R.color.blue));
+        }
         super.onCreate(savedInstanceState);
         binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
         //full screen
